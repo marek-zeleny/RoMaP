83a9fa46ed94b5d7e5cef4d0532b45b2ab89c01d
Marek Zeleny <zeleny.marek@seznam.cz>
2020-08-25 21:12:41 +0200

    Change physical quantities from Kilometers and Hours to Meters and Seconds.

--- RoadTrafficSimulator/Components/PhysicalQuantities.cs
+++ RoadTrafficSimulator/Components/PhysicalQuantities.cs
@@ -2,68 +2,95 @@
 
 namespace RoadTrafficSimulator.Components
 {
-    struct Kilometers
+    struct Meters
     {
-        private readonly double value;
+        private readonly uint value;
 
-        public Kilometers(double value)
+        public Meters(uint value)
         {
             this.value = value;
         }
 
-        public static implicit operator double(Kilometers km) => km.value;
+        public static implicit operator uint(Meters m) => m.value;
 
-        public static explicit operator Kilometers(double d) => new Kilometers(d);
+        public static explicit operator Meters(uint i) => new Meters(i);
 
-        public static KilometersPerHour operator /(Kilometers k, Hours h) => new KilometersPerHour((int)(k.value / h));
+        public static Meters operator +(Meters m1, Meters m2) => new Meters(m1.value + m2.value);
+
+        public static Meters operator -(Meters m1, Meters m2)
+        {
+            if (m1 < m2)
+                throw new ArithmeticException(string.Format("The resulting value of {0} cannot be negative.", nameof(Meters)));
+            return new Meters(m1.value - m2.value);
+        }
+
+        public static MetersPerSecond operator /(Meters m, Seconds s) => new MetersPerSecond(m.value / s);
 
         public override string ToString() => value.ToString();
     }
 
-    struct Hours
+    struct Seconds
     {
-        private readonly double value;
+        private readonly uint value;
 
-        public Hours(double value)
+        public Seconds(uint value)
         {
             this.value = value;
         }
 
-        public static implicit operator double(Hours h) => h.value;
+        public static implicit operator uint(Seconds s) => s.value;
 
-        public static explicit operator Hours(double d) => new Hours(d);
+        public static explicit operator Seconds(uint i) => new Seconds(i);
 
-        public static Kilometers operator *(Hours h, KilometersPerHour kph) => new Kilometers(h.value * kph);
+        public static Seconds operator +(Seconds s1, Seconds s2) => new Seconds(s1.value + s2.value);
 
-        public static Kilometers operator *(KilometersPerHour kph, Hours h) => h * kph;
+        public static Seconds operator -(Seconds s1, Seconds s2)
+        {
+            if (s1 < s2)
+                throw new ArithmeticException(string.Format("The resulting value of {0} cannot be negative.", nameof(Seconds)));
+            return new Seconds(s1.value - s2.value);
+        }
+
+        public static Meters operator *(Seconds s, MetersPerSecond mps) => new Meters(s.value * mps);
+
+        public static Meters operator *(MetersPerSecond mps, Seconds s) => s * mps;
 
         public override string ToString() => value.ToString();
     }
 
-    struct KilometersPerHour
+    struct MetersPerSecond
     {
-        private readonly int value;
+        private readonly uint value;
 
-        public KilometersPerHour(int value)
+        public MetersPerSecond(uint value)
         {
             this.value = value;
         }
 
-        public static implicit operator int(KilometersPerHour kph) => kph.value;
+        public static implicit operator uint(MetersPerSecond mps) => mps.value;
 
-        public static explicit operator KilometersPerHour(int i) => new KilometersPerHour(i);
+        public static explicit operator MetersPerSecond(uint i) => new MetersPerSecond(i);
 
-        public static Hours operator /(Kilometers k, KilometersPerHour kph) => new Hours(k / kph.value);
+        public static MetersPerSecond operator +(MetersPerSecond mps1, MetersPerSecond mps2) => new MetersPerSecond(mps1.value + mps2.value);
+
+        public static MetersPerSecond operator -(MetersPerSecond mps1, MetersPerSecond mps2)
+        {
+            if (mps1 < mps2)
+                throw new ArithmeticException(string.Format("The resulting value of {0} cannot be negative.", nameof(MetersPerSecond)));
+            return new MetersPerSecond(mps1.value - mps2.value);
+        }
+
+        public static Seconds operator /(Meters m, MetersPerSecond mps) => new Seconds(m / mps.value);
 
         public override string ToString() => value.ToString();
     }
 
     static class Extensions
     {
-        public static Kilometers Kilometers(this double d) => new Kilometers(d);
+        public static Meters Meters(this uint i) => new Meters(i);
 
-        public static Hours Hours(this double d) => new Hours(d);
+        public static Seconds Seconds(this uint i) => new Seconds(i);
 
-        public static KilometersPerHour KilometersPerHour(this int i) => new KilometersPerHour(i);
+        public static MetersPerSecond MetersPerSecond(this uint i) => new MetersPerSecond(i);
     }
 }
