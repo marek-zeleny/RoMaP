97931e65048ee4f6625d897e0d8e1ffede2fa2ef
Marek Zeleny <zeleny.marek@seznam.cz>
2021-04-05 11:45:01 +0200

    Add different detail levels for statistics.

--- RoadTrafficSimulator/Statistics/StatisticsBase.cs
+++ RoadTrafficSimulator/Statistics/StatisticsBase.cs
@@ -6,11 +6,50 @@ namespace RoadTrafficSimulator.Statistics
 {
     abstract class StatisticsBase
     {
+        public enum DetailLevel
+        {
+            Low,
+            Medium,
+            High
+        };
+
+        public static DetailLevel detailSettings = DetailLevel.Medium;
+
         protected IClock clock;
 
         protected StatisticsBase(IClock clock)
         {
             this.clock = clock;
+        }
+
+        public struct Item<T>
+        {
+            private T data;
+            public DetailLevel Detail { get; }
+
+            public Item(DetailLevel detail, T data = default)
+            {
+                Detail = detail;
+                if (detail < detailSettings)
+                    data = default;
+                this.data = data;
+            }
+
+            public T Get()
+            {
+                if (Detail >= detailSettings)
+                    return data;
+                else
+                    return default;
+            }
+
+            public void Set(T value)
+            {
+                if (Detail >= detailSettings)
+                    data = value;
+            }
+
+            public static implicit operator T(Item<T> item) => item.Get();
         }
 
         public struct Timestamp<T>
