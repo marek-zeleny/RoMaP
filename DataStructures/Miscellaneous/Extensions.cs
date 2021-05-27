014a00fdea07a51950b9fc84c73372154a71446d
Marek Zeleny <zeleny.marek@seznam.cz>
2021-05-27 15:34:11 +0200

    Add LinkedList.Reverse extension.

---
+++ DataStructures/Miscellaneous/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.Miscellaneous
+{
+    /// <summary>
+    /// Contains extensions of standard .NET as well as custom data structures.
+    /// </summary>
+    public static class Extensions
+    {
+        public static IEnumerable<T> Reverse<T>(this LinkedList<T> list)
+        {
+            var node = list.Last;
+            while (node != null)
+            {
+                yield return node.Value;
+                node = node.Previous;
+            }
+        }
+    }
+}
