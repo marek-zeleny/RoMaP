7701c536fc563643ecb4f2ad1c214b5896654da1
Marek Zeleny <zeleny.marek@seznam.cz>
2022-04-23 23:42:38 +0200

    Add documentation comments to ListSlice and extensions in DataStructures.

--- DataStructures/Miscellaneous/Extensions.cs
+++ DataStructures/Miscellaneous/Extensions.cs
@@ -8,6 +8,16 @@ namespace DataStructures.Miscellaneous
     /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Enumerates elements of <paramref name="list"/> in the reversed order.
+        /// </summary>
+        /// <remarks>
+        /// This extension method is provided to allow lazy evaluation of getting a reversed linked list, which is not
+        /// supported by <see cref="System.Linq.Enumerable.Reverse{TSource}(IEnumerable{TSource})"/>. It also avoids
+        /// unnecessarily traversing the list twice.
+        /// </remarks>
+        /// <param name="list">Linked list to be inverted</param>
+        /// <returns>Sequence of elements in <paramref name="list"/> in reversed order</returns>
         public static IEnumerable<T> Reverse<T>(this LinkedList<T> list)
         {
             var node = list.Last;
--- DataStructures/Miscellaneous/ListSlice.cs
+++ DataStructures/Miscellaneous/ListSlice.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 namespace DataStructures.Miscellaneous
 {
+    /// <summary>
+    /// Read-only slice of a list.
+    /// </summary>
     public class ListSlice<T>
         : IReadOnlyList<T>
     {
@@ -22,9 +25,20 @@ namespace DataStructures.Miscellaneous
 
         public int Count { get; }
 
+        /// <summary>
+        /// Creates a new slice of <paramref name="list"/> from <paramref name="index"/> to the end of the list.
+        /// </summary>
+        /// <param name="list">Original list to create the slice from</param>
+        /// <param name="index">Starting index of the slice</param>
         public ListSlice(IReadOnlyList<T> list, int index)
             : this(list, index, list.Count - index) { }
 
+        /// <summary>
+        /// Creates a new slice of <paramref name="list"/> starting at <paramref name="index"/> spanning
+        /// <paramref name="count"/> elements.
+        /// </summary>
+        /// <param name="count">Number of elements in the slice</param>
+        /// <inheritdoc cref="ListSlice{T}(IReadOnlyList{T}, int)"/>
         public ListSlice(IReadOnlyList<T> list, int index, int count)
         {
             if (index < 0 || index > list.Count)
@@ -36,8 +50,25 @@ namespace DataStructures.Miscellaneous
             Count = count;
         }
 
+        /// <summary>
+        /// Creates a new slice from this slice from <paramref name="index"/> to the end of this slice.
+        /// </summary>
+        /// <param name="index">Starting index of the new slice</param>
+        /// <returns>New slice sharing the original list with the current slice</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="index"/> is out of range of the original slice.
+        /// </exception>
         public ListSlice<T> Slice(int index) => Slice(index, Count - index);
 
+        /// <summary>
+        /// Creates a new slice from this slice starting at <paramref name="index"/> spanning <paramref name="count"/>
+        /// elements.
+        /// </summary>
+        /// <param name="count">Number of elements in the new slice</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="index"/> is out of range of the original slice or <paramref name="count"/> is too high.
+        /// </exception>
+        /// <inheritdoc cref="Slice(int)"/>
         public ListSlice<T> Slice(int index, int count)
         {
             if (index > Count)
@@ -58,8 +89,10 @@ namespace DataStructures.Miscellaneous
 
     public static class ListSliceExtensions
     {
+        /// <inheritdoc cref="ListSlice{T}(IReadOnlyList{T}, int)"/>
         public static ListSlice<T> Slice<T>(this IReadOnlyList<T> list, int index) => new(list, index);
 
+        /// <inheritdoc cref="ListSlice{T}(IReadOnlyList{T}, int, int)"/>
         public static ListSlice<T> Slice<T>(this IReadOnlyList<T> list, int index, int count) =>
             new(list, index, count);
     }
